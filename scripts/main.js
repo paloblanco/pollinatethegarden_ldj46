e9869e384f834f42bf70b90fa4ba320a5247a36f
paloblanco <panella.rocco@gmail.com>
2020-04-18 17:06:50 -0400

    Drawing scene in Threejs

--- scripts/main.js
+++ scripts/main.js
@@ -92,6 +92,89 @@ context.canvas.style.width = '${targetwidth}px'
 // context.scale(basescale*pixelRatio, basescale*pixelRatio);
 context.imageSmoothingEnabled = false;
 
+/*
+################
+THREEJS SETUP
+################
+*/
+
+// these need to be accessed inside more than one function so we'll declare them first
+let container;
+let camera;
+let renderer;
+let scene;
+let mesh;
+
+// OFFSCREEN TARGET FOR 3D drawing
+const rtWidth = 400;
+const rtHeight = 240;
+const renderTarget = new THREE.WebGLRenderTarget(rtWidth, rtHeight);
+
+function init() {
+    // Get a reference to the container element that will hold our scene
+    // container = document.querySelector( '#container' );
+    // container = document.querySelector( '#container' );
+
+    // create a Scene
+    scene = new THREE.Scene();
+    // set the background color
+    scene.background = new THREE.Color(listColorsHex[12]);
+
+    //make the camera
+    // Create a Camera
+    const fov = 30; // AKA Field of View
+    const aspect = basewidth / baseheight;
+    const near = 0.1; // the near clipping plane
+    const far = 100; // the far clipping plane
+    camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+
+    // every object is initially created at ( 0, 0, 0 )
+    // we'll move the camera back a bit so that we can view the scene
+    camera.position.set( 0, 0, 10 );
+    // the above line is equivalent to doing the following:
+    // camera.position.x = 0;
+    // camera.position.y = 0;
+    // camera.position.z = 10;
+
+    // create a geometry
+    const geometry = new THREE.BoxBufferGeometry( 2, 2, 2 );
+
+    // create a purple Basic material
+    const material = new THREE.MeshStandardMaterial( { color: listColorsHex[2] } );
+    const material2 = new THREE.MeshBasicMaterial( { color: listColorsHex[2] } );
+
+    // create a Mesh containing the geometry and material
+    mesh = new THREE.Mesh( geometry, material );
+    mesh2 = new THREE.Mesh( geometry, material2 );
+
+    // add the mesh to the scene
+    scene.add( mesh );
+    scene.add( mesh2 );
+    mesh2.position.set(3,0,0);
+
+    //add a light to the scene
+    const light = new THREE.DirectionalLight(0xffffff, 5.0);
+
+    //move the light, since its default position is 000
+    light.position.set(0,3,3);
+
+    //add the light to the scene
+    scene.add(light);
+
+    // create a renderer
+    renderer = new THREE.WebGLRenderer({antialias: true});
+
+    renderer.setSize( basewidth, baseheight );
+    // renderer.setPixelRatio( window.devicePixelRatio );
+
+    // add the automatically created <canvas> element to the page
+    // container.appendChild( renderer.domElement );
+    // renderer.setRenderTarget(ctx);
+}
+
+init();
+
+
 
 /*
 ################
@@ -269,7 +352,14 @@ UPDATE
 */
 
 update = function() {
-    
+  // animate our cube a little bit
+  mesh.rotation.z += 0.01;
+  mesh.rotation.y += 0.01;
+  mesh.rotation.x += 0.01;
+
+  mesh2.rotation.z += 0.01;
+  mesh2.rotation.y += 0.01;
+  mesh2.rotation.x += 0.01; 
     
 
     }
@@ -283,7 +373,7 @@ DRAW
 draw = function() {
 
     ctx.fillStyle = listColors[6];
-    ctx.fillRect(0, 0, 400, 240);// x, y, width, height
+    // ctx.fillRect(0, 0, 400, 240);// x, y, width, height
     ctx.fillStyle = listColors[0];
     
 
@@ -291,8 +381,20 @@ draw = function() {
     print("Its Alive!", 11,12);
       
     // context.scale(basescale*pixelRatio, basescale*pixelRatio);
-    context.drawImage(offCanvas,0,0,pixelRatio*basescale*400,pixelRatio*basescale*240);
+
+    
+
+    // render, or 'create a still image', of the scene
+    // this will create one still image / frame each time the animate
+    // function calls itself
+    renderer.render( scene, camera );
+
+    
     // context.scale(1, 1);
+
+    context.drawImage(renderer.domElement,0,0,pixelRatio*basescale*400,pixelRatio*basescale*240);
+
+    context.drawImage(offCanvas,0,0,pixelRatio*basescale*400,pixelRatio*basescale*240);
 
     
 }
@@ -304,18 +406,46 @@ MAIN LOOP
 */
 
 
+
+animate = function() {
+  // call animate recursively
+  requestAnimationFrame( animate );
+
+  // animate our cube a little bit
+  mesh.rotation.z += 0.01;
+  mesh.rotation.y += 0.01;
+  mesh.rotation.x += 0.01;
+
+  mesh2.rotation.z += 0.01;
+  mesh2.rotation.y += 0.01;
+  mesh2.rotation.x += 0.01;
+
+  // render, or 'create a still image', of the scene
+  // this will create one still image / frame each time the animate
+  // function calls itself
+  renderer.render( scene, camera );
+}
+
+
+
 loop = function() {
 
   update();
   draw();
 
-    // if (i==3) {
-      
-    //   draw();
-    // }
+  // if (i==3) {
+    
+  //   draw();
+  // }
 
-    // call update when the browser is ready to draw again
-    window.requestAnimationFrame(loop);
+  
+
+  // context.drawImage(renderTarget.texture.mipmaps,0,0,pixelRatio*basescale*400,pixelRatio*basescale*240);
+
+
+
+  // call update when the browser is ready to draw again
+  window.requestAnimationFrame(loop);
 
 
 
